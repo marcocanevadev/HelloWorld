ab7bd5369547d9e6f8438ef78bc0365ea2b623a4
marco caneva <marco.caneva.dev@outlook.com>
2023-04-20 21:16:04 +0200

    improved experience

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (20.0f));
+    g.setFont (juce::Font (60.0f));
     g.setColour (juce::Colours::powderblue);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("GITHUB CIAO peppe", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
